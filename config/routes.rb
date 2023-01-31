bdf0818a228d8249e5e4da016561b1f6199d7ff5
linhdinhlhp <linhdinhlhpk97@gmail.com>
2023-01-31 09:10:19 +0000

    add book review

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     
+  resources :book_reviews
   get 'users/show'
   get 'static_pages/home'
   root 'static_pages#home'
@@ -7,4 +8,17 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  class CreateBookReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :book_reviews do |t|
+      t.integer :user_id
+      t.integer :book_id
+      t.text :review
+
+      t.timestamps
+    end
+  end
+end
+  
 end
